a97c5600adf9760f950731bc0c610c5450b6e3b6
Guillaume Delignieres <Guillaume Delignieres>
2017-02-17 10:34:46 -0500

    FirstCOmmit bis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1>
+             <h2>{{hero.name}} details</h2>
+             <div><label>id :</label>{{hero.id}}</div>
+             <div>
+              <label>name: </label>
+              <input [(ngModel)]="hero.name" placeholder="name">
+            </div>`
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1, name: 'windstorm'};
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
